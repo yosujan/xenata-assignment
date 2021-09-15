2dc0cc905cb9b597339f72a2605047881e0927e7
yosujan <yosujan@gmail.com>
2021-09-16 01:27:39 +0200

    updated http common

--- src/services/HttpCommon.js
+++ src/services/HttpCommon.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_URL = "https://685rp9jkj1.execute-api.eu-west-1.amazonaws.com/prod/";
-const X_API_KEY = "***REMOVED***";
+const X_API_KEY = process.env.REACT_APP_X_API_KEY;
 
 export default axios.create({
 	baseURL: API_URL,
